5a7d99047f40790e02c14368be0c69aa48dab5bc
Adifa saniya <153834311+adifa-7@users.noreply.github.com>
2024-12-23 15:46:58 +0530

    Add files via upload

--- sample.java
+++ sample.java
@@ -1,117 +1,155 @@
-/*package yasasvi;
+/*creation jdbc*/
+/*
+package javas;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
+
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
+
 import java.sql.Statement;
 
 public class sample {
 
-    public static void main(String[] args) {
-        try {
-            // Step1: register the driver
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+	public static void main(String[]args) 
 
-            // Step2: get the connection object
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05f5", "root", "1234");
+	{
+		try {
+			//Step1: register the driver   Student s=new Student()
 
-            // Step3: create statement object which is used to send SQL queries to database
-            Statement statement = connection.createStatement();
+			//Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Step4: execute the query
-            ResultSet rs = statement.executeQuery("SELECT year, subject, winner, country FROM nobel_prizes");
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 
-            // Step5: process the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("year") + "\t" + rs.getString("subject") + "\t" + rs.getString("winner") + "\t" + rs.getString("country"));
-            }
+			//step2: get the connection object
 
-            // Step6: close the connection
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
-package yasasvi;
+			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05g3", "root", "1234");
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+			//step3: create statement object which is used to send SQL queries to database
 
-public class sample {
+			Statement statement=connection.createStatement();
 
-    public static void main(String[] args) {
-        try {
-            // Step1: register the driver
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			//step4: execute the query
 
-            // Step2: get the connection object
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05f5", "root", "1234");
+			ResultSet rs=statement.executeQuery("select *from Students");
 
-            // Step3: create statement object which is used to send SQL queries to database
-            Statement statement = connection.createStatement();
+			while(rs.next()) {
 
-            // Step4: execute the query
-            ResultSet rs = statement.executeQuery("SELECT year FROM nobel_prizes");
+				System.out.println(rs.getString("StudentID")+"\t"+rs.getString("FirstName")+"\t"+rs.getString("LastName")+"\t"+rs.getString("BirthDate")+"\t"+rs.getString("Email"));
 
-            // Step5: process the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("year"));
-            }
+			}
 
-            // Step6: close the connection
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			//step5: close the connection
+
+			connection.close();
+
+		} catch (Exception e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 }
 
-package yasasvi;
+*/
+/*insertion*/
+/*
+package javas;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
+
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
 
 public class sample {
 
     public static void main(String[] args) {
         try {
-            
+
+            // Step1: Register the driver
+
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05f5", "root", "1234");
-            String insertQuery = "INSERT INTO nobel_prizes (year, subject, winner, country) VALUES (?, ?, ?, ?)";
+
+            // Step2: Get the connection object
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05g3", "root", "1234");
+
+            // Step3: Create a PreparedStatement object
+
+            String insertQuery = "INSERT INTO Students (StudentID, FirstName, LastName, BirthDate, Email) VALUES (?, ?, ?, ?, ?)";
+
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-            preparedStatement.setInt(1, 2025);
-            preparedStatement.setString(2, "Physics");
-            preparedStatement.setString(3, "Emma Brown");
-            preparedStatement.setString(4, "Australia");
-            preparedStatement.setInt(1, 2027);
-            preparedStatement.setString(2, "Computer  Science");
-            preparedStatement.setString(3, "Yasasvi");
-            preparedStatement.setString(4, "India");
+
+            // Step4: Set the parameters
+            
+            preparedStatement.setInt(1, 4);
+
+            preparedStatement.setString(2, "lakshmi");
+
+            preparedStatement.setString(3, "yasasvi");
+
+            preparedStatement.setString(4, "2005-04-05");
+
+            preparedStatement.setString(5, "yasasvi@gmail.com");
+
+            preparedStatement.setInt(1, 5);
+            
+            preparedStatement.setString(2, "adifa");
+
+            preparedStatement.setString(3, "saniya");
+
+            preparedStatement.setString(4, "2005-08-27");
+
+            preparedStatement.setString(5, "adifasaniya@gmail.com");
+
+            // Step5: Execute the update
             
             int rowsAffected = preparedStatement.executeUpdate();
+
             System.out.println("Rows affected: " + rowsAffected);
-            String selectQuery = "SELECT year, subject, winner, country FROM nobel_prizes";
+
+            // Fetch and print the data to verify the insert
+
+            String selectQuery = "SELECT * FROM Students";
+
             ResultSet rs = preparedStatement.executeQuery(selectQuery);
+
             while (rs.next()) {
-                System.out.println(rs.getInt("year") + "\t" + rs.getString("subject") + "\t" + rs.getString("winner") + "\t" + rs.getString("country"));
+
+				System.out.println(rs.getString("StudentID")+"\t"+rs.getString("FirstName")+"\t"+rs.getString("LastName")+"\t"+rs.getString("BirthDate")+"\t"+rs.getString("Email"));
+
             }
 
-            
+            // Step6: Close the connection
+
             connection.close();
+
         } catch (SQLException e) {
-            e.printStackTrace();
+
+        	e.printStackTrace();
+
         }
+
     }
-}*/
-package yasasvi;
+
+}  
+
+*/
+/* insert and update */
+
+package javas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -127,43 +165,45 @@ public class sample {
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 
             // Step2: get the connection object
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05f5", "root", "1234");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05g3", "root", "1234");
 
             // Step3: create a PreparedStatement object for insertion
-            String insertQuery = "INSERT INTO nobel_prizes (year, subject, winner, country) VALUES (?, ?, ?, ?)";
+            String insertQuery = "INSERT INTO Students (StudentID, FirstName, LastName, BirthDate, Email) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
 
             // Set the parameters for the insertion
-            insertStatement.setInt(1, 2028);
-            insertStatement.setString(2, "Computer Science");
-            insertStatement.setString(3, "Adifa");
-            insertStatement.setString(4, "India");
+            insertStatement.setInt(1, 11);
+            insertStatement.setString(2, "me");
+            insertStatement.setString(3, "you");
+            insertStatement.setString(4, "2005-05-03");
+            insertStatement.setString(5, "me.you@gmail.com");
 
             // Execute the insertion
             int insertRowsAffected = insertStatement.executeUpdate();
             System.out.println("Insertion - Rows affected: " + insertRowsAffected);
 
             // Step4: create a PreparedStatement object for update
-            String updateQuery = "UPDATE nobel_prizes SET winner = ?, country = ? WHERE year = ? AND subject = ?";
+            String updateQuery = "UPDATE Students SET StudentID = ?, FirstName = ?, LastName = ?, BirthDate = ? WHERE Email = ?";
             PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
 
             // Set the parameters for the update
-            updateStatement.setString(1, "Updated Winner");
-            updateStatement.setString(2, "Updated Country");
-            updateStatement.setInt(3, 2025); // Specify the year
-            updateStatement.setString(4, "Physics"); // Specify the subject
+            updateStatement.setInt(1, 7);
+            updateStatement.setString(2, "bangtan");
+            updateStatement.setString(3, "Soneyeondan");
+            updateStatement.setString(4, "2013-06-13");
+            updateStatement.setString(5, "michael.brown@example.com");
 
             // Execute the update
             int updateRowsAffected = updateStatement.executeUpdate();
             System.out.println("Update - Rows affected: " + updateRowsAffected);
 
             // Step5: fetch and print the data to verify the insert and update
-            String selectQuery = "SELECT year, subject, winner, country FROM nobel_prizes";
+            String selectQuery = "SELECT * FROM Students";
             PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
             ResultSet rs = selectStatement.executeQuery();
 
             while (rs.next()) {
-                System.out.println(rs.getInt("year") + "\t" + rs.getString("subject") + "\t" + rs.getString("winner") + "\t" + rs.getString("country"));
+                System.out.println(rs.getString("StudentID") + "\t" + rs.getString("FirstName") + "\t" + rs.getString("LastName") + "\t" + rs.getString("BirthDate") + "\t" + rs.getString("Email"));
             }
 
             // Step6: close the connection
@@ -173,5 +213,3 @@ public class sample {
         }
     }
 }
-
-
