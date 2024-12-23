efc41ce93bbf17025d82ef10a9a14531bc1308df
Bhanu-Sri-Rathod <154400326+Bhanu-Sri-Rathod@users.noreply.github.com>
2024-12-23 15:59:54 +0530

    Add files via upload

--- insert,update,delete.java
+++ insert,update,delete.java
@@ -13,12 +13,30 @@ public class Sample {
         String password = "1234";
         String tableName = "sample";
 
+        String insertQuery = "INSERT INTO " + tableName + " (id, sname, rating, age) VALUES (1, 'Alice', 5, 25.5)";
+        String updateQuery = "UPDATE " + tableName + " SET rating = 4 WHERE id = 1";
+        String deleteQuery = "DELETE FROM " + tableName + " WHERE id = 1";
         String selectQuery = "SELECT * FROM " + tableName;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
+
+            int rowsAffected = statement.executeUpdate(insertQuery);
+            if (rowsAffected > 0) {
+                System.out.println("Record inserted successfully!");
+            }
+
+            rowsAffected = statement.executeUpdate(updateQuery);
+            if (rowsAffected > 0) {
+                System.out.println("Record updated successfully!");
+            }
+
+            rowsAffected = statement.executeUpdate(deleteQuery);
+            if (rowsAffected > 0) {
+                System.out.println("Record deleted successfully!");
+            }
 
             ResultSet resultSet = statement.executeQuery(selectQuery);
             int columnCount = resultSet.getMetaData().getColumnCount();
