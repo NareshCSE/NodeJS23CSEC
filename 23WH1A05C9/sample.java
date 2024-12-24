95cc7cd250fdde4a63ff3265f9e17b92918c0056
siddi-Harshini <154400547+siddi-Harshini@users.noreply.github.com>
2024-12-24 12:23:10 +0530

    Add files via upload

---
+++ 23WH1A05C9/sample.java
@@ -0,0 +1,57 @@
+package sample;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+
+import java.sql.Statement;
+
+
+
+public class sample {
+
+	public static void main(String[]args) throws SQLException {
+        // loading driver class
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+		//connection object
+
+		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/sample","root","1234");
+
+		
+        //statement object
+		Statement statement=connection.createStatement();
+
+		
+        //executing query
+		ResultSet rs=statement.executeQuery("select * from employees");
+
+		//resultset
+		while(rs.next()) {
+
+			System.out.println(rs.getString("ename")+"\t"+rs.getInt("eid"));
+
+		}
+
+		connection.close();
+
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
