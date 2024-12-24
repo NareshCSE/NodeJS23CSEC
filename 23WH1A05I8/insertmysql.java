9f74ac256dc0988297279d000518898d8d886bb6
Srividyai8 <153827897+Srividyai8@users.noreply.github.com>
2024-12-24 11:12:21 +0530

    Create insertmysql.java

---
+++ 23WH1A05I8/insertmysql.java
@@ -0,0 +1,31 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class insertmysql {
+
+	public static void main(String[] args) {
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/23wh1a05i8","root","1234");
+			Statement statement=connection.createStatement();
+			String insert="insert into employee (id,name,salary) values(9,'saniya',800000) ";
+			int rowsinserted =statement.executeUpdate(insert);
+			if (rowsinserted > 0) {
+				System.out.println("A new employee record was inserted successfully.");
+	            	}
+			ResultSet rs = statement.executeQuery("SELECT * FROM employee WHERE id = 9");
+	        while (rs.next()) {
+	        	System.out.println("Inserted Data:");
+	            System.out.println("id: " + rs.getString("id") +", Name: " + rs.getString("name") +", Salary: " + rs.getString("salary"));
+	            }
+	        connection.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
