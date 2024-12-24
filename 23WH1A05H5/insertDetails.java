2aa3c8e56a0b2f2b6dd77f40b09bcef2b537bd53
Lohitha Markapuram <153831557+Lohithamarkapuram@users.noreply.github.com>
2024-12-24 11:06:08 +0530

    Add files via upload

---
+++ 23WH1A05H5/insertDetails.java
@@ -0,0 +1,31 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class insertDetails {
+	public static void main(String[] args) {
+		try {
+			Date date = new Date(20090629);
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/235h5", "root", "1234");
+			String sql="insert into employee values(?,?,?,?,?,?)";
+			PreparedStatement statement = con.prepareStatement(sql);
+			statement.setInt(1, 4);
+			statement.setString(2,"Sherlock");
+			statement.setString(3, "Holmes");
+			statement.setString(4, "holmes@gmail.com");
+			statement.setDate(5,date);
+			statement.setString(6,"Sales");
+			int rowsInsert=statement.executeUpdate();
+			if(rowsInsert>0) {
+				System.out.println("1 row inserted");
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
