b9f0c904fba53e4bc15b3ec777098ced43e16ca0
nagasharmada <154654784+nagasharmada@users.noreply.github.com>
2024-12-24 11:35:38 +0530

    RETRIEVE_JDBC.java

--- 23WH1A05H1/RETRIEVE_JDBC.java
+++ 23WH1A05H1/RETRIEVE_JDBC.java
@@ -2,6 +2,7 @@ package mypack;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -10,11 +11,14 @@ public static void main(String[] args) {
 	try {
 		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbms","root","1234");
-		Statement state=conn.createStatement();
-		ResultSet rs=state.executeQuery("select * from employ");
+		//Statement state=conn.createStatement();
+		PreparedStatement ps=conn.prepareStatement("select eid from employ where ename=?");
+		ps.setString(1,"Domu");
+		ResultSet rs=ps.executeQuery();
 		while(rs.next()) {
-			System.out.println("Eid"+rs.getInt("eid"));
-			System.out.println("Ename"+rs.getString("ename"));
+			
+			System.out.println("Eid "+rs.getInt("eid"));
+		//	System.out.println("Ename"+rs.getString("ename"));
 		}
 		conn.close();
 	}
