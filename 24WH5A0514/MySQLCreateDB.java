846c6be1069b24c1416758aa13277e5ee3c5fa28
SriAkarsha <24wh5a0514@bvrithyderabad.edu.in>
2024-12-24 11:35:45 +0530

    Update and rename MySQLInsert.java to MySQLCreateDB.java

--- 24WH5A0514/MySQLCreateDB.java
+++ 24WH5A0514/MySQLCreateDB.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class demo {
+public class createtable {
 public static void main(String[] args)
 {
 try {
