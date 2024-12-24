be5a558c5160418332071b8a077cc828b2a360fb
nagasharmada <154654784+nagasharmada@users.noreply.github.com>
2024-12-24 11:36:07 +0530

    Add files via upload

---
+++ 23WH1A05H1/CREATE_JDBC.java
@@ -0,0 +1,25 @@
+package mypack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class CREATE_JDBC {
+public static void main(String[] args) {
+	try {
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbms","root","1234");
+		Statement state=conn.createStatement();
+		ResultSet rs=state.executeQuery("select * from employ");
+		while(rs.next()) {
+			System.out.println("Eid"+rs.getInt("eid"));
+			System.out.println("Ename"+rs.getString("ename"));
+		}
+		conn.close();
+	}
+	catch(Exception e) {
+		System.out.println("Excepttion occured: "+e);
+	}
+}
+}
