aa08d5dde9bc7249344f96655fbc9651316c9f58
Ch Harika Reddy <154301279+Harika-07@users.noreply.github.com>
2024-12-30 13:29:51 +0530

    Create update.java

---
+++ 23WH1A05H0 /update.java
@@ -0,0 +1,34 @@
+package JavaPackage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Update {
+
+    public static void main(String[] args) {
+
+        try {
+            // Step1: register the driver
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            // Step2: get the connection object
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sample", "root", "1234");
+
+            // Step3: create statement object which is used to send SQL queries to database
+            Statement statement = connection.createStatement();
+
+            // Step4: execute the query
+            // Changed to executeUpdate for an INSERT query
+            int rowsAffected = statement.executeUpdate("update Employee set dept='wkbt' where eid=1");
+            System.out.println(rowsAffected + " row(s) inserted successfully.");
+
+            // Step5: close the connection
+            connection.close();
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
