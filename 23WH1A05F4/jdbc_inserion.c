57b984f52cd99b4c020e4bee367fe5c018a2ab33
Tamkinf4 <154491872+Tamkinf4@users.noreply.github.com>
2024-12-31 11:25:18 +0530

    Create jdbc_inserion.c

---
+++ 23WH1A05F4/jdbc_inserion.c
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class insertjdbcexample {
+	public static void main(String[]args) throws SQLException {
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+		
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/sample","root","1234");
+		
+		Statement statement=conn.createStatement();
+		
+		String Insertquery="insert into employees values(6,'Harshu','cse',150000)";
+		
+		int rowsaffected=statement.executeUpdate(Insertquery);
+		
+		if(rowsaffected>0) {
+			System.out.println(rowsaffected + " rows affected");
+		}
+		
+		ResultSet rs=statement.executeQuery("select * from employees");
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt("eid")+"\t"+rs.getString("ename")+"\t"+rs.getString("dept")+"\t"+rs.getInt("salary"));
+		}
+		conn.close();		
+		
+	}
+}
