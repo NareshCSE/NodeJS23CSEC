9752c07185ac392f020c090597773bfa9c8b8da5
G.Rishika <154901687+rishika1205@users.noreply.github.com>
2025-03-11 10:25:25 +0530

    Result.jsx

---
+++ 23WH1A05H9/ReactDemo/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Result = () => {
+  return (
+    <div className="Result">
+      <h1>Thanks for your Feedback</h1>
+
+    </div>
+  );
+};
+
+export default Result;
