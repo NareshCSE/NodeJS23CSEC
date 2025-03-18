9c89c1e246280538afb3762c122e8d23156f31dd
Shreya Pulla <154301275+Shreya5i4@users.noreply.github.com>
2025-03-18 11:43:33 +0530

    Add files via upload

---
+++ 23WH1A05I4/ e-book/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 My E-Commerce Store</p>
+    </footer>
+  );
+}
+
+export default Footer;
