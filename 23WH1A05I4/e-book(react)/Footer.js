4a8b07cf33a7aab35bd526389ff951a2d4d46b87
Shreya Pulla <154301275+Shreya5i4@users.noreply.github.com>
2025-03-18 09:41:35 +0530

    Create Footer.js

---
+++ 23WH1A05I4/e-book(react)/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 CSE BVRITH ECOMMERCE. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
