d68da83b6548d4013f4df580d4e20d8155340f0a
sohanasri1806 <153828487+sohanasri1806@users.noreply.github.com>
2025-03-18 19:44:32 +0530

    Create App.jsx

---
+++ 23WH1A05H7/App.jsx
@@ -0,0 +1,15 @@
+//API
+import React from "react";
+
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <div className="App">
+      <About />
+      </div>
+  );
+};
+
+
+export default App;
