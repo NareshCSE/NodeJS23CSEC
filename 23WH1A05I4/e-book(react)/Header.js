6e262c910e448da4661427bf4cd6f3fc1a322c55
Shreya Pulla <154301275+Shreya5i4@users.noreply.github.com>
2025-03-18 09:42:28 +0530

    Create Header.js

---
+++ 23WH1A05I4/e-book(react)/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="th.jpeg" className="logo" alt="Logo" />
+            <h1>CSE BVRITH ECOMMERCE</h1>
+        </header>
+    );
+};
+
+export default Header;
