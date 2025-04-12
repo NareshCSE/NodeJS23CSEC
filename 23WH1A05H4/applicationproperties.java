b894e7d41a33c124618324befea4d43775a1beeb
ManyaSree05 <154441766+ManyaSree05@users.noreply.github.com>
2025-04-12 23:46:36 +0530

    Create applicationproperties.java

---
+++ 23WH1A05H4/applicationproperties.java
@@ -0,0 +1,17 @@
+spring.application.name=ebookbe
+
+spring.datasource.url=jdbc:mysql://localhost:3306/library
+
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+spring.datasource.password=1234
+
+spring.datasource.username=root
+
+server.port=8085
+
+spring.jpa.show-sql=true
+
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect
+
+spring.jpa.hibernate.ddl-auto=update
