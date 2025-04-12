a1f610c1c08fa252494167f640a1ffe3cfec0654
nagasharmada <154654784+nagasharmada@users.noreply.github.com>
2025-04-12 18:38:59 +0530

    Add files via upload

---
+++ 23WH1A05H1/Book.java
@@ -0,0 +1,40 @@
+package com.example.ebook;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String author;
+	private String desc;
+	 public Book() {}
+
+	    public Book(String title, String author, String desc) {
+	        this.title = title;
+	        this.author = author;
+	        this.desc = desc;
+	    }
+
+	    // Getters & Setters
+	    public Long getId() { return id; }
+	    public void setId(Long id) { this.id = id; }
+
+	    public String getTitle() { return title; }
+	    public void setTitle(String title) { this.title = title; }
+
+	    public String getAuthor() { return author; }
+	    public void setAuthor(String author) { this.author = author; }
+
+	    public String getDesc() { return desc; }
+	    public void setDesc(String d) {
+	    	this.desc=d;
+	    }
+
+}
