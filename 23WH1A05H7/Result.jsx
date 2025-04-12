f9e51560d0452aca2d98ad58430ca096626061d7
sohanasri1806 <153828487+sohanasri1806@users.noreply.github.com>
2025-04-12 22:34:12 +0530

    Create Result.jsx

---
+++ 23WH1A05H7/Result.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Result = () => {
+  return (
+    <div className="result">
+      <h1>Thanks for your feedback!</h1>
+    </div>
+  );
+};
+
+export default Result;
+
+
