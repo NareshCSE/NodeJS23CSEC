36a1b6fd09c7ac05ba9c7c459abffea3b10d0643
SruthiSrivatsavayi <153825022+SruthiSrivatsavayi@users.noreply.github.com>
2025-04-12 15:40:26 +0530

    Create bookx.java

---
+++ 23WH1A05H3/bookx.java
@@ -0,0 +1,17 @@
+package com.example.ebook.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String description;
+    private String cover;
+    private String content;
+}
