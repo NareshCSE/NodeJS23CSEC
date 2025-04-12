127b8bd4423199bd5e2b74562f436a02f40c6891
Tamkinf4 <154491872+Tamkinf4@users.noreply.github.com>
2025-04-12 16:31:09 +0530

    Create daobook.java

---
+++ 23WH1A05F4/daobook.java
@@ -0,0 +1,7 @@
+package com.bvrith.ebook.ebookbe.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bvrith.ebook.ebookbe.model.Book;
+
+public interface BookDAO extends JpaRepository<Book, Integer> {
+}
