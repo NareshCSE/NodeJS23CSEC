bce7e419e97f80d5dcbb2c213abb344a4ab8324b
ManyaSree05 <154441766+ManyaSree05@users.noreply.github.com>
2025-04-12 23:43:33 +0530

    Create cart.jsx

---
+++ 23WH1A05H4/cart.jsx
@@ -0,0 +1,13 @@
+// src/pages/Cart.js
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-semibold">Your Cart</h2>
+      <p className="mt-2">No items in cart.</p>
+    </div>
+  );
+};
+
+export default Cart;
