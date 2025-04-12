0ea7c24ee024a24ff86ace794373157e19998797
Saraswathi <24wh5a0515@bvrithyderabad.edu.in>
2025-04-12 23:06:11 +0530

    Create Updatequery.java

---
+++ 24WH5A0515/Updatequery.java
@@ -0,0 +1,37 @@
+package myproject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class update {
+public static void main(String[] args)
+
+{
+try {
+
+DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "1234");
+Statement statement=connection.createStatement();
+
+			String updateQuery = "UPDATE employee SET role = 'Manager' WHERE ename = 'Teja'";
+			int rowsUpdated = statement.executeUpdate(updateQuery);
+			System.out.println(rowsUpdated + " row(s) updated.");
+
+ResultSet rs=statement.executeQuery("select *from employee");
+
+while(rs.next()) {
+
+System.out.println(rs.getString("eid")+"\t"+rs.getString("ename")+"\t"+rs.getString("salary")+"\t"+rs.getString("role"));
+
+}
+connection.close();
+
+} catch (Exception e) {
+
+e.printStackTrace();
+
+}
+}
+}
