94c99d2cd1e90641d61ae1cdfc4954242dc3a384
ManyaSree05 <154441766+ManyaSree05@users.noreply.github.com>
2025-04-12 23:49:16 +0530

    Create EbookController.java

---
+++ 23WH1A05H4/EbookController.java
@@ -0,0 +1,137 @@
+package com.bvrith.ebook.ebookbe.controller;
+
+
+
+import java.util.List;
+
+import java.util.Optional;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+import com.bvrith.ebook.ebookbe.dao.BookDao;
+
+import com.bvrith.ebook.ebookbe.model.Book;
+
+
+
+@RestController
+
+@RequestMapping("/books")
+
+public class EBookController {
+
+
+
+    @Autowired
+
+    private BookDao bookDoa;
+
+
+
+    // GET all books
+
+    @GetMapping
+
+    public List<Book> getAllBooks() {
+
+        return bookDoa.findAll();
+
+    }
+
+
+
+    // GET book by ID
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<Book> getBookById(@PathVariable Integer id) {
+
+        Optional<Book> book = bookDoa.findById(id);
+
+        return book.map(ResponseEntity::ok)
+
+                   .orElse(ResponseEntity.notFound().build());
+
+    }
+
+
+
+    // POST (Create a new book)
+
+    @PostMapping
+
+    public Book createBook(@RequestBody Book book) {
+
+        return bookDoa.save(book);
+
+    }
+
+
+
+    // PUT (Update an existing book)
+
+    @PutMapping("/{id}")
+
+    public ResponseEntity<Book> updateBook(@PathVariable Integer id, @RequestBody Book bookDetails) {
+
+        Optional<Book> bookOptional = bookDoa.findById(id);
+
+
+
+        if (bookOptional.isPresent()) {
+
+            Book book = bookOptional.get();
+
+            book.setName(bookDetails.getName());
+
+            book.setPrice(bookDetails.getPrice());
+
+            book.setDetails(bookDetails.getDetails());
+
+            Book updatedBook = bookDoa.save(book);
+
+            return ResponseEntity.ok(updatedBook);
+
+        } else {
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+    }
+
+
+
+    // DELETE a book by ID
+
+    @DeleteMapping("/{id}")
+
+    public ResponseEntity<Void> deleteBook(@PathVariable Integer id) {
+
+        Optional<Book> bookOptional = bookDoa.findById(id);
+
+
+
+        if (bookOptional.isPresent()) {
+
+            bookDoa.deleteById(id);
+
+            return ResponseEntity.noContent().build();
+
+        } else {
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+    }
+
+}
