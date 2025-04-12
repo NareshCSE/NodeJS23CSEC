bdf3316c35fbe3e1e4ddc9207eb71153e0faf8ae
Saraswathi <24wh5a0515@bvrithyderabad.edu.in>
2025-04-12 22:25:51 +0530

    app.jsx

---
+++ 24WH5A0515/app.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter,Routes,Route} from "react-router";
+import AppLayout from "./components/layout/AppLayout/AppLayout"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Result from "./pages/Result";
+const App = () =>{
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path ='/*' element={<AppLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path = 'Home' element ={<Home/>}/>
+      <Route path = 'About' element = {<About/>}/>
+      <Route path="Services" element={<Services/>}/>
+      <Route path="Contact" element={<Contact/>}/>
+      <Route path="Result" element={<Result/>}/>
+      </Route>
+      
+    </Routes>
+    </BrowserRouter>
+  
+  )
+}
+
+export default App;
