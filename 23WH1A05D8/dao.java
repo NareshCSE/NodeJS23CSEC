9cd1af715404f10cb8d30da3bae1f3b8acddae39
Vaishnavi Kotturi <154414648+K-Vaishnavi10@users.noreply.github.com>
2025-04-13 22:59:25 +0530

    Create Dao.java

---
+++ 23WH1A05D8/dao.java
@@ -0,0 +1,13 @@
+package com.bvrith.dao;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bvrith.demo.model.Book;
+
+
+
+public interface Dao extends JpaRepository<Book, Integer> {
+
+}
