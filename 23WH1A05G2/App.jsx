7b297df1b0735c13ba9725fa96b47e7aab5a2569
Yasasvi2006 <153826647+Yasasvi2006@users.noreply.github.com>
2025-04-14 20:43:57 +0530

    Add files via upload

--- 23WH1A05G2/App.jsx
+++ 23WH1A05G2/App.jsx
@@ -1,45 +1,40 @@
-//defining a functional component 
-import {useState} from 'react' //for useState fn
-import "./App.css" //to import the css App
-//state:an object that would store info/data in component
+import { BrowserRouter, Routes ,Route} from "react-router"
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Registration from "./pages/Registartion"
+import AppLayout from "./components/layout/AppLayout/AppLayout"
+import Result from "./pages/Result"
+import Result2 from "./pages/Result2"
+import Login from "./pages/Login"
+import Result3 from "./pages/Result3"
+import Cart from "./pages/Cart"
+//BrowserRouter, Routes ,Route to move from 1 page to other
+//route -path,element
 
-const App=()=>{ //arrow function
-  const [value1,setValue1]=useState() 
-  const [value2,setValue2]=useState()
-  const [result,setResult]=useState()
+const App= () => {
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path='/*' element={<AppLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='home' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='services' element={<Services/>}/>
+      <Route path='Contact' element={<Contact/>}/>
+      <Route path='registration' element={<Registration/>}/>
+      <Route path='result' element={<Result/>}/>
+      <Route path='result2' element={<Result2/>}/>
+      <Route path='result3' element={<Result3/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='cart' element={<Cart/>}/>
 
-  //definition for handeChange
-  const handleChange=(event)=>{
-    console.log(event.target.value)
-    setValue1(event.target.value)
-  }
-  const handleChange1=(event)=>{
-    setValue2(event.target.value)
-  }
-  //instead of two or many function we can usse:
-  
-  /*const handleChange=(event , type)=>{
-    if(type=='value1')
-      setValue1(event.target.value)
-    else if(type==='value2')
-      setValue2(event.target.value)
-}*/
-  const handleClick=()=>{
-    let v1=parseInt(value1)
-    let v2=parseInt(value2)
-    setResult(v1+v2)
-  }
 
-  return ( //jsx format inside paranthesis. 
-    <> {/*empty tag is fragement*/}
-      <h1>Hello yasasvi!!</h1>
-      
-    <input type="text" placeholder="input1" onChange={handleChange}/> {/*onChange={(e)=>handleChange(e,'value1')} -callback fn :in onchange*/}
-    <input type="text" placeholder="input2"onChange={handleChange1}/> 
-    <button onClick={handleClick}>ADD</button>
-    <p>Answer:{result}</p> {/* paragraph gives new line */}
-    </>
+      </Route>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App // to let the main know about the app 
+export default App
