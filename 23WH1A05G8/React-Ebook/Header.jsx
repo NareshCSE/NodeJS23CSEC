9a56d45cc0ea0bc4c952d6463c986cba038a487e
Sreshta21 <153832096+Sreshta21@users.noreply.github.com>
2025-04-14 12:33:03 +0530

    Create Header.jsx

---
+++ 23WH1A05G8/React-Ebook/Header.jsx
@@ -0,0 +1,38 @@
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #333;
+    color: #fff;
+    border-radius: 12px;
+  }
+  .website-name{
+    font-family: 'Poppins';
+    font-size:1.5rem;
+    color:rgb(223,209,80);
+    text-align:left;
+  }
+  .logo img {
+    height: 100px;
+    width: 100px;
+  }
+  
+  .nav ul {
+    list-style: none;
+    display: flex;
+    gap: 20px;
+  }
+  
+  .nav ul li {
+    margin: 0;
+  }
+  
+  .nav ul li a {
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .nav ul li a:hover {
+    text-decoration: underline;
+  }
