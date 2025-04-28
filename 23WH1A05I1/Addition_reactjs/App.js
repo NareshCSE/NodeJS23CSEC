2cdb650dc4d694a598ceae335c9646fb5db846ea
Tejasreei1 <154443497+Tejasreei1@users.noreply.github.com>
2025-04-28 23:11:17 +0530

    Create App.js

---
+++ 23WH1A05I1/Addition_reactjs/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SumCalculator from './SumCalculator'; // Important: Import the component
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Sum Calculator App!</h1>
+      <SumCalculator />
+    </div>
+  );
+}
+
+export default App;
