71dec1c6a716392708fa693da0d19267c8e8178b
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2025-04-29 13:13:12 +0530

    Create Employee.java

---
+++ 23WH1A05G4/ext/Employee.java
@@ -0,0 +1,24 @@
+package com.example.external;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+    private double salary;
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+}
