3723fba0891f03e44a7eed13d7fcec64117210cc
Praneetha167 <154442872+Praneetha167@users.noreply.github.com>
2025-04-29 12:55:39 +0530

    Create ExternalApp.js

---
+++ 23WH1A05F2/ExternalApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeSearch from "./component/EmployeeSearch";
+
+function App() {
+return (
+ <div>
+   <EmployeeSearch />
+ </div>
+);
+}
+
+export default App;
