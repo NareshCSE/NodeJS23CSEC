50e0837f4c2fe8bea5448623f3870b806348c7cc
siddi-Harshini <154400547+siddi-Harshini@users.noreply.github.com>
2025-04-29 11:25:16 +0530

    Update app.js

--- 23WH1A05C9/External/frontend/app.js
+++ 23WH1A05C9/External/frontend/app.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import EmployeeForm from './components/EmployeeForm';
+
+function App() {
+    return (
+        <div>
+            <EmployeeForm />
+        </div>
+    );
+}
+
+export default App;
 
